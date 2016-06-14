05477fb201228c2ca5139be93821de802f768e80
okandiyebiri <okansibut@gmail.com>
2016-06-15 02:05:04 +0300

    Değişiklik
    Anasayfada ki yazı içeriği sola yaslandı.
    SQL dosyası değiştirildi.

--- SQL.sql
+++ SQL.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.2.11
+-- version 4.3.11
 -- http://www.phpmyadmin.net
 --
 -- Anamakine: 127.0.0.1
--- Üretim Zamanı: 05 Mar 2015, 06:13:30
--- Sunucu sürümü: 5.6.21
--- PHP Sürümü: 5.5.19
+-- Üretim Zamanı: 15 Haz 2016, 01:03:00
+-- Sunucu sürümü: 5.6.24
+-- PHP Sürümü: 5.6.8
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -27,7 +27,7 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE IF NOT EXISTS `ayarlar` (
-`id` int(11) NOT NULL,
+  `id` int(11) NOT NULL,
   `adsoyadi` varchar(50) COLLATE utf8_bin NOT NULL,
   `kullaniciadi` varchar(50) COLLATE utf8_bin NOT NULL,
   `emailadresi` varchar(100) COLLATE utf8_bin NOT NULL,
@@ -46,7 +46,7 @@ CREATE TABLE IF NOT EXISTS `ayarlar` (
 --
 
 INSERT INTO `ayarlar` (`id`, `adsoyadi`, `kullaniciadi`, `emailadresi`, `telefonnumarasi`, `siteadresi`, `sifre`, `ilksayfabaslik`, `ilksayfaicerik`, `sitebaslik`, `siteaciklama`, `anahtarkelime`) VALUES
-(1, 'Okan IÅžIK', 'admin', 'okansibut@gmail.com', '+90 546 863 21 60', 'http://okandiyebiri.com', '12345', 'Aramadan Ã¶nce kendinize nedenini sorun !', '<p><span style="color:rgb(102, 102, 102); font-family:cabin,sans-serif; font-size:16px">Bu sayfa Cv ama&ccedil;lÄ± yapÄ±lmÄ±ÅŸtÄ±r. G&uuml;ncel yazÄ± ve videolu anlatÄ±mlara ulaÅŸabilmek i&ccedil;in&nbsp;</span><a href="http://okandiyebiri.com/" style="border: 0px; font-family: Cabin, sans-serif; font-size: 16px; margin: 0px; outline: 0px; padding: 0px; vertical-align: baseline; color: rgb(22, 165, 215); line-height: 20px; text-align: center;">ziyaret edin.</a></p>\r\n', 'OkanÄ±n Sitesi', 'OkanÄ±n Sitesi Ã‡ok GÃ¼zel Olacakk', 'okan, anahtar, anahtarlÄ±k, Ã§amlÄ±k, mallÄ±k');
+(1, 'Okan IŞIK', 'admin', 'okansibut@gmail.com', '+90 546 863 21 60', 'http://okandiyebiri.com', '12345', 'İlk sayfanızın başlığı', '<p><em><strong>Lorem ipsum dolor sit amet, consectetur</strong></em></p>\r\n\r\n<p style="text-align:justify"><img alt="" src="https://avatars1.githubusercontent.com/u/19917975?v=3&amp;s=460" style="float:left; height:230px; margin:10px; width:230px" />Adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>\r\n\r\n<p style="text-align:justify">Duis aute irure dolor in <strong>reprehenderit</strong> in voluptate <span style="color:#FFF0F5"><strong><span style="background-color:#4B0082">velit esse cillum</span></strong></span> dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, <span style="color:#008080"><strong>sunt in culpa qui officia</strong></span> deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur <strong><span style="color:#FF0000">adipisicing elit</span></strong>, sed do eiusmod tempor <a href="https://okandiyebiri.com">okan ışık</a> ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis <span style="background-color:#FF8C00">nostrud</span> exercitation.</p>\r\n', 'Okan''ın Sitesi', 'Herşey Çok Güzel Olacak', 'okandiyebiri, okan ışık');
 
 -- --------------------------------------------------------
 
@@ -55,7 +55,7 @@ INSERT INTO `ayarlar` (`id`, `adsoyadi`, `kullaniciadi`, `emailadresi`, `telefon
 --
 
 CREATE TABLE IF NOT EXISTS `bilgiler` (
-`id` int(11) NOT NULL,
+  `id` int(11) NOT NULL,
   `adi_soyadi` varchar(255) COLLATE utf8_bin NOT NULL,
   `telefon` varchar(255) COLLATE utf8_bin NOT NULL,
   `siteadresi` varchar(255) COLLATE utf8_bin NOT NULL,
@@ -76,7 +76,7 @@ INSERT INTO `bilgiler` (`id`, `adi_soyadi`, `telefon`, `siteadresi`, `emailadres
 --
 
 CREATE TABLE IF NOT EXISTS `deneyimler` (
-`id` int(11) NOT NULL,
+  `id` int(11) NOT NULL,
   `baslamatarihi` date NOT NULL,
   `ayrilmatarihi` date NOT NULL,
   `calistigiyer` varchar(250) COLLATE utf8_bin NOT NULL,
@@ -89,9 +89,7 @@ CREATE TABLE IF NOT EXISTS `deneyimler` (
 --
 
 INSERT INTO `deneyimler` (`id`, `baslamatarihi`, `ayrilmatarihi`, `calistigiyer`, `gorevi`, `aciklama`) VALUES
-(2, '2011-01-01', '2015-02-14', 'Ã‡ETIN GROUP (TEKIRDAÄž)', 'Ã–ZEL GÃœVENLIK GÃ–REVLISI', '<p>G&ouml;rev tanÄ±mÄ±: 51188 sayÄ±lÄ± kanunu kapsayan maddeler. SÄ±rasÄ±yla g&ouml;rev yaptÄ±ÄŸÄ± yerleri;</p>\r\n\r\n<p>&Ouml;zel Elektrolitik(TekirdaÄŸ)</p>\r\n\r\n<p>Doluca Åžarap&ccedil;Ä±lÄ±k(TekirdaÄŸ)</p>\r\n\r\n<p>Teknogon HMY Group(TekirdaÄŸ)</p>\r\n\r\n<p>Elpa Kimya(TekirdaÄŸ)</p>\r\n\r\n<p>Roter Tekstil BaskÄ± ve Rotasyon Ä°ÅŸletmeleri(TekirdaÄŸ)</p>\r\n'),
-(3, '2010-01-01', '2011-01-01', 'AYSO CATERING (TEKIRDAÄž)', 'ÅžEF', '<p>Salon d&uuml;zeni, eksik tespit ve tedariÄŸi G&ouml;rev yaptÄ±ÄŸÄ± yer;</p>\r\n\r\n<p>Sel HortumlarÄ±</p>\r\n'),
-(4, '2005-01-01', '2008-01-01', 'ALSANCAK BAYRAK (Ä°STANBUL)', 'Ã‡IZIMCI', '<p><span style="background-color:rgb(252, 252, 252); color:rgb(102, 102, 102); font-family:cabin,sans-serif; font-size:16px">Bayrak &ccedil;izimi, dikime hazÄ±rlanmasÄ±, paketleme, kargo ve g&ouml;nderimi, stok d&uuml;zeni</span></p>\r\n');
+(4, '2005-01-01', '2016-01-01', 'OKANDIYEBIRI', 'Webmaster', '<p><span style="background-color:rgb(252, 252, 252); color:rgb(102, 102, 102); font-family:cabin,sans-serif; font-size:16px">Backend, Frontend</span></p>\r\n');
 
 -- --------------------------------------------------------
 
@@ -100,7 +98,7 @@ INSERT INTO `deneyimler` (`id`, `baslamatarihi`, `ayrilmatarihi`, `calistigiyer`
 --
 
 CREATE TABLE IF NOT EXISTS `egitim` (
-`id` int(11) NOT NULL,
+  `id` int(11) NOT NULL,
   `baslamatarihi` date NOT NULL,
   `bitirmetarihi` date NOT NULL,
   `okulduzeyi` varchar(150) COLLATE utf8_bin NOT NULL,
@@ -113,10 +111,7 @@ CREATE TABLE IF NOT EXISTS `egitim` (
 --
 
 INSERT INTO `egitim` (`id`, `baslamatarihi`, `bitirmetarihi`, `okulduzeyi`, `bolum`, `aciklama`) VALUES
-(4, '2012-01-01', '2017-01-01', 'Sertifika', 'GÃ¼venlik', '<p><span style="background-color:rgb(247, 247, 247); color:rgb(102, 102, 102); font-family:cabin,sans-serif; font-size:16px">SilahsÄ±z &Ouml;zel G&uuml;venlik G&ouml;revlisi EÄŸitimi | Ä°lkyardÄ±m EÄŸitimi</span></p>\r\n'),
-(5, '2015-02-04', '2015-02-05', 'YabancÄ± Dil', 'Ä°ngilizce', '<p><span style="background-color:rgb(252, 252, 252); color:rgb(102, 102, 102); font-family:cabin,sans-serif; font-size:16px">Okuma: Ä°yi | Yazma: Ä°yi | Anlama: Orta | KonuÅŸma: Orta Herhangi bir eÄŸitim almadÄ±m ancak kullandÄ±ÄŸÄ±m kodlamalar ve programlar beni her ge&ccedil;en g&uuml;n bu konuda kelime daÄŸarcÄ±ÄŸÄ± a&ccedil;Ä±sÄ±ndan geliÅŸtiriyor.</span></p>\r\n'),
-(6, '2008-01-01', '2009-01-01', 'Askerlik ile iliÅŸiÄŸi', 'YapÄ±ldÄ±', '<p><span style="background-color:rgb(247, 247, 247); color:rgb(102, 102, 102); font-family:cabin,sans-serif; font-size:16px">K&uuml;tahya, Mersin ve Erzurum&#39;da Hv.P.Er olarak yapÄ±ldÄ±.</span></p>\r\n'),
-(7, '1993-01-01', '2005-01-01', 'Lise', 'SÃ¶zel', '<p>BaÄŸcÄ±lar Orhan Gazi Lisesi, Bafra KÄ±zÄ±lÄ±rmak Lisesi</p>\r\n\r\n<p>Bafra Barbaros Ä°lkokulu, Bafra Nazmiye Demirel Ä°lk&ouml;ÄŸretim Okulu</p>\r\n');
+(7, '1993-01-01', '2005-01-01', 'Lise', 'Sözel', '<p>Bağcılar Orhan Gazi Lisesi</p>\r\n');
 
 -- --------------------------------------------------------
 
@@ -125,22 +120,24 @@ INSERT INTO `egitim` (`id`, `baslamatarihi`, `bitirmetarihi`, `okulduzeyi`, `bol
 --
 
 CREATE TABLE IF NOT EXISTS `resim` (
-`id` int(11) NOT NULL,
+  `id` int(11) NOT NULL,
   `resim` varchar(255) COLLATE utf8_bin NOT NULL,
   `altbaslik` varchar(250) COLLATE utf8_bin NOT NULL,
   `ustbaslik` varchar(250) COLLATE utf8_bin NOT NULL,
   `aciklama` varchar(250) COLLATE utf8_bin NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=57 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+) ENGINE=InnoDB AUTO_INCREMENT=67 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 --
 -- Tablo döküm verisi `resim`
 --
 
 INSERT INTO `resim` (`id`, `resim`, `altbaslik`, `ustbaslik`, `aciklama`) VALUES
-(43, 'okandiyebiri32350', 'SCDCS', 'CSCSC', 'portfolio_post filter1 filter3'),
-(44, 'okandiyebiri68584', 'SCSCS', 'sCSCS', 'portfolio_post filter2 filter1'),
-(45, 'okandiyebiri26277', 'SCSCSCSCS', 'sCSCSSCS', 'portfolio_post filter3 filter2'),
-(56, 'okandiyebiri89307', 'fssdf', 'sfsdfsdf', 'portfolio_post filter1 filter3');
+(60, 'okandiyebiri1784', 'wdwdwa', 'dadadada', 'portfolio_post filter1 filter3'),
+(61, 'okandiyebiri99383', 'sdda', 'asdasda', 'portfolio_post filter2 filter1'),
+(62, 'okandiyebiri56406', 'asdasd', 'sada', 'portfolio_post filter3 filter2'),
+(63, 'okandiyebiri59726', 'axsaad', 'asdasda', 'portfolio_post filter1 filter3'),
+(64, 'okandiyebiri80748', 'dasdasd', 'dadasa', 'portfolio_post filter1 filter3'),
+(66, 'okandiyebiri46629', 'gdgdfgd', 'fgdfgdgd', 'portfolio_post filter2 filter1');
 
 -- --------------------------------------------------------
 
@@ -149,7 +146,7 @@ INSERT INTO `resim` (`id`, `resim`, `altbaslik`, `ustbaslik`, `aciklama`) VALUES
 --
 
 CREATE TABLE IF NOT EXISTS `sag_yetenekler` (
-`id` int(11) NOT NULL,
+  `id` int(11) NOT NULL,
   `yetenekler` varchar(250) COLLATE utf8_bin NOT NULL,
   `nekadariyisin` varchar(3) COLLATE utf8_bin NOT NULL,
   `renkkodu` varchar(250) COLLATE utf8_bin NOT NULL
@@ -172,7 +169,7 @@ INSERT INTO `sag_yetenekler` (`id`, `yetenekler`, `nekadariyisin`, `renkkodu`) V
 --
 
 CREATE TABLE IF NOT EXISTS `sosyal` (
-`id` int(11) NOT NULL,
+  `id` int(11) NOT NULL,
   `link` varchar(250) COLLATE utf8_bin NOT NULL,
   `iconno` varchar(6) COLLATE utf8_bin NOT NULL,
   `icontur` varchar(1) COLLATE utf8_bin NOT NULL,
@@ -197,7 +194,7 @@ INSERT INTO `sosyal` (`id`, `link`, `iconno`, `icontur`, `renk`) VALUES
 --
 
 CREATE TABLE IF NOT EXISTS `yetenekler` (
-`id` int(11) NOT NULL,
+  `id` int(11) NOT NULL,
   `yetenekler` varchar(100) COLLATE utf8_bin NOT NULL,
   `nekadariyisin` varchar(4) COLLATE utf8_bin NOT NULL,
   `renkkodu` varchar(200) COLLATE utf8_bin NOT NULL
@@ -222,49 +219,49 @@ INSERT INTO `yetenekler` (`id`, `yetenekler`, `nekadariyisin`, `renkkodu`) VALUE
 -- Tablo için indeksler `ayarlar`
 --
 ALTER TABLE `ayarlar`
- ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Tablo için indeksler `bilgiler`
 --
 ALTER TABLE `bilgiler`
- ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Tablo için indeksler `deneyimler`
 --
 ALTER TABLE `deneyimler`
- ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Tablo için indeksler `egitim`
 --
 ALTER TABLE `egitim`
- ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Tablo için indeksler `resim`
 --
 ALTER TABLE `resim`
- ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Tablo için indeksler `sag_yetenekler`
 --
 ALTER TABLE `sag_yetenekler`
- ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Tablo için indeksler `sosyal`
 --
 ALTER TABLE `sosyal`
- ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Tablo için indeksler `yetenekler`
 --
 ALTER TABLE `yetenekler`
- ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Dökümü yapılmış tablolar için AUTO_INCREMENT değeri
@@ -274,42 +271,42 @@ ALTER TABLE `yetenekler`
 -- Tablo için AUTO_INCREMENT değeri `ayarlar`
 --
 ALTER TABLE `ayarlar`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
 --
 -- Tablo için AUTO_INCREMENT değeri `bilgiler`
 --
 ALTER TABLE `bilgiler`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=31;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=31;
 --
 -- Tablo için AUTO_INCREMENT değeri `deneyimler`
 --
 ALTER TABLE `deneyimler`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=5;
 --
 -- Tablo için AUTO_INCREMENT değeri `egitim`
 --
 ALTER TABLE `egitim`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=8;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=8;
 --
 -- Tablo için AUTO_INCREMENT değeri `resim`
 --
 ALTER TABLE `resim`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=57;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=67;
 --
 -- Tablo için AUTO_INCREMENT değeri `sag_yetenekler`
 --
 ALTER TABLE `sag_yetenekler`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=9;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=9;
 --
 -- Tablo için AUTO_INCREMENT değeri `sosyal`
 --
 ALTER TABLE `sosyal`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=12;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=12;
 --
 -- Tablo için AUTO_INCREMENT değeri `yetenekler`
 --
 ALTER TABLE `yetenekler`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=17;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=17;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
