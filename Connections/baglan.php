f8bf41168f5d5f614cb4fe299697e623a00d335f
okandiyebiri <okansibut@gmail.com>
2016-06-14 15:54:12 +0300

    Veritabanı Türkçe Karakter Hatası Giderildi

--- Connections/baglan.php
+++ Connections/baglan.php
@@ -7,4 +7,7 @@ $database_baglan = "yonetimpaneli";
 $username_baglan = "root";
 $password_baglan = "";
 $baglan = mysql_pconnect($hostname_baglan, $username_baglan, $password_baglan) or trigger_error(mysql_error(),E_USER_ERROR); 
+
+mysql_query("SET CHARACTER SET 'utf8'");
+mysql_query("SET NAMES 'utf8'");
 ?>
