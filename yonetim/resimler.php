b19c6544be509c3789bf8253ff468916a1e85af9
okandiyebiri <okansibut@gmail.com>
2016-06-17 02:34:02 +0300

    Resim Yükleme Sayfa Düzenlendi

--- yonetim/resimler.php
+++ yonetim/resimler.php
@@ -38,29 +38,26 @@ if (isset($_SERVER['QUERY_STRING'])) {
 
 if ((isset($_POST["MM_insert"])) && ($_POST["MM_insert"] == "form")) {
   $insertSQL = sprintf("INSERT INTO resim (resim, altbaslik, ustbaslik, aciklama) VALUES (%s, %s, %s, %s)",
-                       GetSQLValueString($_POST['resim'], "file"),
-                       GetSQLValueString($_POST['altbaslik'], "text"),
-                       GetSQLValueString($_POST['ustbaslik'], "text"),
-                       GetSQLValueString($_POST['aciklama'], "text"));
+   GetSQLValueString($_POST['resim'], "file"),
+   GetSQLValueString($_POST['altbaslik'], "text"),
+   GetSQLValueString($_POST['ustbaslik'], "text"),
+   GetSQLValueString($_POST['aciklama'], "text"));
 
   mysql_select_db($database_baglan, $baglan);
   $Result1 = mysql_query($insertSQL, $baglan) or die(mysql_error());
 }
 ?>
 <?php
-//initialize the session
 if (!isset($_SESSION)) {
   session_start();
 }
 
-// ** Logout the current user. **
 $logoutAction = $_SERVER['PHP_SELF']."?doLogout=true";
 if ((isset($_SERVER['QUERY_STRING'])) && ($_SERVER['QUERY_STRING'] != "")){
   $logoutAction .="&". htmlentities($_SERVER['QUERY_STRING']);
 }
 
 if ((isset($_GET['doLogout'])) &&($_GET['doLogout']=="true")){
-  //to fully log out a visitor we need to clear the session varialbles
   $_SESSION['MM_Username'] = NULL;
   $_SESSION['MM_UserGroup'] = NULL;
   $_SESSION['PrevUrl'] = NULL;
@@ -82,22 +79,15 @@ if (!isset($_SESSION)) {
 $MM_authorizedUsers = "";
 $MM_donotCheckaccess = "true";
 
-// *** Restrict Access To Page: Grant or deny access to this page
 function isAuthorized($strUsers, $strGroups, $UserName, $UserGroup) { 
-  // For security, start by assuming the visitor is NOT authorized. 
   $isValid = False; 
 
-  // When a visitor has logged into this site, the Session variable MM_Username set equal to their username. 
-  // Therefore, we know that a user is NOT logged in if that Session variable is blank. 
   if (!empty($UserName)) { 
-    // Besides being logged in, you may restrict access to only certain users based on an ID established when they login. 
-    // Parse the strings into arrays. 
     $arrUsers = Explode(",", $strUsers); 
     $arrGroups = Explode(",", $strGroups); 
     if (in_array($UserName, $arrUsers)) { 
       $isValid = true; 
     } 
-    // Or, you may restrict access to only certain users based on their username. 
     if (in_array($UserGroup, $arrGroups)) { 
       $isValid = true; 
     } 
@@ -121,17 +111,13 @@ if (!((isset($_SESSION['MM_Username'])) && (isAuthorized("",$MM_authorizedUsers,
 }
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml"><!-- InstanceBegin template="/Templates/panel.dwt.php" codeOutsideHTMLIsLocked="false" -->
+<html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<!-- InstanceBeginEditable name="doctitle" -->
 <title>Yönetim Paneli</title>
-<!-- InstanceEndEditable -->
 <link href="../SpryAssets/SpryMenuBarVertical.css" rel="stylesheet" type="text/css" />
 <link rel="stylesheet" href="../css/style.css">
 <link rel="stylesheet" href="../css/bootstrap.min.css">
-<!-- InstanceBeginEditable name="head" -->
-<!-- InstanceEndEditable -->
 </head>
 <body>
 	<div class="navbar navbar-inverse navbar-static-top">
@@ -159,23 +145,35 @@ if (!((isset($_SESSION['MM_Username'])) && (isAuthorized("",$MM_authorizedUsers,
 		</div>
 	</div>
     <div class="container">
-<!-- InstanceBeginEditable name="EditRegion1" -->
-<form name="form" action="resim-ekle.php" enctype="multipart/form-data" method="POST"> 
-<select name="aciklama">
-        <option value="portfolio_post filter1 filter3" <?php if (!(strcmp("portfolio_post filter1 filter3", ""))) {echo "SELECTED";} ?>>Başta</option>
-        <option value="portfolio_post filter2 filter1" <?php if (!(strcmp("portfolio_post filter2 filter1", ""))) {echo "SELECTED";} ?>>Ortada</option>
-        <option value="portfolio_post filter3 filter2" <?php if (!(strcmp("portfolio_post filter3 filter2", ""))) {echo "SELECTED";} ?>>Sonda</option>
-</select>
-<input type="text" name="altbaslik"/>
-<input type="text" name="ustbaslik"/>
-<input type="file" name="resim"/>
-
-<input type="submit" class="buton" value="Kaydet" />
-<input type="hidden" name="MM_insert" value="form" />
-</form>  
-    <!-- InstanceEndEditable -->
-
+		<form name="form" action="resim-ekle.php" enctype="multipart/form-data" method="POST"> 
+			<table class="table table-bordered table-responsive">
+				<thead bgcolor="#46b8da" style="color:white;">
+					<tr>
+						<th>Yön</th>
+						<th>Alt Başlık</th>
+						<th colspan="3">Üst Başlık</th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr>
+						<td>
+						<select name="aciklama" class="form-control">
+							<option value="portfolio_post filter1 filter3" <?php if (!(strcmp("portfolio_post filter1 filter3", ""))) {echo "SELECTED";} ?>>Başta</option>
+							<option value="portfolio_post filter2 filter1" <?php if (!(strcmp("portfolio_post filter2 filter1", ""))) {echo "SELECTED";} ?>>Ortada</option>
+							<option value="portfolio_post filter3 filter2" <?php if (!(strcmp("portfolio_post filter3 filter2", ""))) {echo "SELECTED";} ?>>Sonda</option>
+						</select>
+						</td>
+						<td><input type="text" class="form-control" name="altbaslik" required/></td>
+						<td><input type="text" class="form-control" name="ustbaslik" required/></td>
+						<td><input type="file" class="form-control" name="resim"/></td>
+						<td><input type="submit" class="btn btn-warning" value="Kaydet" /></td>
+						<input type="hidden" name="MM_insert" value="form" />
+					</tr>
+				</tbody>
+			</table>
+		</form>  
     </div>
+
     <div class="container">
            <div class="alert alert-warning alert-dismissible" role="alert">
           <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
@@ -192,4 +190,4 @@ if (!((isset($_SESSION['MM_Username'])) && (isAuthorized("",$MM_authorizedUsers,
 <script src="http://code.jquery.com/jquery-1.10.2.min.js"></script>
 <script src="../js/bootstrap.min.js"></script>
 </body>
-<!-- InstanceEnd --></html>
+</html>
