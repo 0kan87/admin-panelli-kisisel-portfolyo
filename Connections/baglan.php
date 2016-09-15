061ab8e63bbdb277a1c7ae44c03db3d7f9cb9c33
okandiyebiri <okansibut@gmail.com>
2016-09-15 15:51:50 +0300

    PDO Hata Kapat
    Pdo hata kapatıldı

--- Connections/baglan.php
+++ Connections/baglan.php
@@ -1,12 +1,9 @@
 <?php
-# FileName="Connection_php_mysql.htm"
-# Type="MYSQL"
-# HTTP="true"
 $hostname_baglan = "localhost";
 $database_baglan = "yonetimpaneli";
 $username_baglan = "root";
 $password_baglan = "";
-$baglan = mysql_pconnect($hostname_baglan, $username_baglan, $password_baglan) or trigger_error(mysql_error(),E_USER_ERROR); 
+@$baglan = mysql_pconnect($hostname_baglan, $username_baglan, $password_baglan) or trigger_error(mysql_error(),E_USER_ERROR); 
 
 mysql_query("SET CHARACTER SET 'utf8'");
 mysql_query("SET NAMES 'utf8'");
